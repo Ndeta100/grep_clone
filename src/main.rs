685243f06988b98cc760f48c562fb0b37fd764a6
Ndeta Innocent <you@example.com>
2022-08-02 15:41:20 +0300

    implementing our struct to m ake it a little nicer

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
     let contents = match fs::read_to_string(&config.filename) {
@@ -19,9 +19,10 @@ struct Config {
     query: String,
     filename: String,
 }
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
