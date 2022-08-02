8a7f8554a35aeed36424ad507f050f8c0ba36b85
Ndeta Innocent <you@example.com>
2022-08-02 15:31:12 +0300

    using a struct to collect the values

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
-    let (query, filename) = parse_config(&args);
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-    let contents = match fs::read_to_string(filename) {
+    let config = parse_config(&args);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    let contents = match fs::read_to_string(&config.filename) {
         Ok(file) => file,
-        Err(e) => panic!("could not read file {} , due to errr {}", filename, e),
+        Err(e) => panic!(
+            "could not read file {} , due to errr {}",
+            config.filename, e
+        ),
     };
     println!("{}", contents);
 }
+struct Config {
+    query: String,
+    filename: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config { query, filename }
 }
