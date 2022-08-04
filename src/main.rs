8d8dcf2f7b55bc21d54d3e9c60157071b262ece3
Ndeta Innocent <you@example.com>
2022-08-04 16:54:21 +0300

    handling errors with the eprintln! macro

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
     if let Err(e) = grep_cli::run(config) {
-        println!("application error {}", e);
+        eprintln!("application error {}", e);
         process::exit(1);
     }
 }
